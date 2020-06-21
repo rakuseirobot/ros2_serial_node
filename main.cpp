e9c5b3513c31e2dd825dd8659fd8d44faab183b5
SoulaS <helloworldwithsoulas@gmail.com>
2020-06-22 06:16:58 +0900

    Change the device name to ttyUSB0 from ttyACM0

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ auto main() -> int {
   boost::asio::io_service io;
 
   // ポートは /dev/ttyACM0
-  boost::asio::serial_port serial(io, "/dev/ttyACM0");
+  boost::asio::serial_port serial(io, "/dev/ttyUSB0");
   // 速度は 9600bps
   serial.set_option(boost::asio::serial_port_base::baud_rate(9600));
 
@@ -15,7 +15,7 @@ auto main() -> int {
   std::this_thread::sleep_for(std::chrono::seconds(5));
 
   // "ping" を送信
-  boost::asio::write(serial, boost::asio::buffer("ping\n"));
+  //boost::asio::write(serial, boost::asio::buffer("ping\n"));
 
   // serial から response_buf に '\n' まで読み込む
   boost::asio::streambuf response_buf;
